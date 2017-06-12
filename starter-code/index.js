32c9730074a50bca922d1e20daa0bc28601705c4
f3r <fer@ironhack.com>
2017-06-12 17:48:29 +0100

    split starter-code, solution code

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,48 +1,10 @@
-var SortedList = function() {
-  this.length = 0;
-  this.items = [];
-};
+var SortedList = function() {};
 
-SortedList.prototype.add = function(item) {
-  this.items.push(item);
-  this.items.sort(function(a,b) {
-    return a-b;
-  });
-  this.length++;
-}
-
-SortedList.prototype.get = function(pos) {
-  if (pos<1 || pos > this.length) {
-    throw new Error('OutOfBounds');
-  }
-  return this.items[pos-1];
-}
-
-SortedList.prototype.max = function() {
-  if (this.length === 0) {
-    throw new Error('EmptySortedList');
-  }
-  return Math.max.apply(Math, this.items);
-}
-
-SortedList.prototype.min = function() {
-  if (this.length === 0) {
-    throw new Error('EmptySortedList');
-  }
-  return Math.min.apply(Math, this.items);
-}
-
-SortedList.prototype.average = function() {
-  return (this.sum() / this.length);
-}
-
-SortedList.prototype.sum = function() {
-  if (this.items.length === 0) {
-    throw new Error('EmptySortedList');
-  }
-  return this.items.reduce(function(sum, ele){
-    return sum + ele;
-  }, 0);
-}
+SortedList.prototype.add     = function(item) {}
+SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.max     = function() {}
+SortedList.prototype.min     = function() {}
+SortedList.prototype.average = function() {}
+SortedList.prototype.sum     = function() {}
 
 module.exports = SortedList;
